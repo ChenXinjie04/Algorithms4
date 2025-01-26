009b7e198521957ad51bd346fe00a58a341dbfce
ChenXinjie <Xinjiechen04@gmail.com>
2025-01-26 13:10:18 +0800

    Link list stack

---
+++ exercises/Ch_1_3/Stack.java
@@ -0,0 +1,42 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class Stack<Item> {
+    private Node first;
+    private int N;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        N++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
